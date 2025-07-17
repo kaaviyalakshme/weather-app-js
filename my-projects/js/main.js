71bb64adf32ea20d88a5c56758d2b8b9ea839e38
kaaviyalakshme <kaaviyalakshme2509@gmail.com>
2025-07-17 12:43:37 +0530

    Create main.js

---
+++ my-projects/js/main.js
@@ -0,0 +1,24 @@
+import { getWeather } from './weatherService.js';
+import { displayWeather } from './uiRenderer.js';
+document.addEventListener('DOMContentLoaded',() => {
+    const form = document.getElementById('weather-form');
+    const cityInput = document.getElementById('city-input');
+
+    form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        const city= cityInput.value.trim();
+
+        if(city){
+            getWeather(city)
+                 .then(data => {
+                    displayWeather(data);
+                 })
+                .catch(error => {
+                    console.error('Error fetching weather data:',error);
+                    displayWeather({main:{temp:'N/A'},weather:[{description : 'Unable to fetch data'}],localSunrise: 'N/A',localSunset:'N/A'});
+                });
+        }else{
+            console.error('City input is empty');
+            displayWeather({main:{temp:'N/A'},weather:[{description : 'City name is required'}],localSunrise: 'N/A',localSunset:'N/A' });
+        };
+    });
